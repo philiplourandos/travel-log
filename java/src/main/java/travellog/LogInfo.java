3d047d72e7248f770ebb604063d0880e235c2e5f
Philip Lourandos <philip.lourandos@gmail.com>
2016-11-25 14:25:53 +0200

    # Implemented compareTo method

--- java/src/main/java/travellog/LogInfo.java
+++ java/src/main/java/travellog/LogInfo.java
@@ -1,11 +1,13 @@
 package travellog;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author plourand
  */
-public class LogInfo {
-    private String timestamp;
+public class LogInfo implements Comparable<LogInfo> {
+    private LocalDateTime timestamp;
     private String gps;
 
     public LogInfo() {
@@ -16,14 +18,23 @@ public class LogInfo {
     }
 
     public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
+        this.timestamp = LocalDateTime.parse(timestamp);
     }
 
     public String getGps() {
         return gps;
     }
 
-    public String getTimestamp() {
+    public LocalDateTime getTimestamp() {
         return timestamp;
+    }
+
+    @Override
+    public int compareTo(LogInfo o) {
+        if (LogInfo.class.isAssignableFrom(o.getClass())) {
+            return timestamp.compareTo(o.timestamp);
+        } else {
+            return -1;
+        }
     }
 }
