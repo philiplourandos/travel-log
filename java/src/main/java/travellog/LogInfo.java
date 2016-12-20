7c9411d4fd04db29c0c099215df7c81a5743d1d2
Philip Lourandos <abpl245@absa.co.za>
2016-12-20 10:45:58 +0200

    DEV
    # Added extra member fields
    # Removed default constructor

--- java/src/main/java/travellog/LogInfo.java
+++ java/src/main/java/travellog/LogInfo.java
@@ -7,28 +7,34 @@ import java.time.LocalDateTime;
  * @author plourand
  */
 public class LogInfo implements Comparable<LogInfo> {
-    private LocalDateTime timestamp;
-    private String gps;
+    private final LocalDateTime timestamp;
+    private final String longitude;
+    private final String latitude;
+    private final String filename;
 
-    public LogInfo() {
-    }
-
-    public void setGps(String gps) {
-        this.gps = gps;
-    }
-
-    public void setTimestamp(String timestamp) {
+    public LogInfo(final String timestamp, final String longitude, final String latitude, final String filename) {
         this.timestamp = LocalDateTime.parse(timestamp);
-    }
-
-    public String getGps() {
-        return gps;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.filename = filename;
     }
 
     public LocalDateTime getTimestamp() {
         return timestamp;
     }
 
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+    
     @Override
     public int compareTo(LogInfo o) {
         if (LogInfo.class.isAssignableFrom(o.getClass())) {
