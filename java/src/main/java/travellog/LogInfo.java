63cc4eff477d1944a02d21d77af044bcf166aadd
Philip Lourandos <abpl245@absa.co.za>
2016-12-20 15:39:59 +0200

    DEV
    # Date format was coming in with ':' in the date portion instead of '-'. Use formatter when parsing

--- java/src/main/java/travellog/LogInfo.java
+++ java/src/main/java/travellog/LogInfo.java
@@ -1,19 +1,22 @@
 package travellog;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
  * @author plourand
  */
 public class LogInfo implements Comparable<LogInfo> {
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss");
+    
     private final LocalDateTime timestamp;
     private final String longitude;
     private final String latitude;
     private final String filename;
 
     public LogInfo(final String timestamp, final String longitude, final String latitude, final String filename) {
-        this.timestamp = LocalDateTime.parse(timestamp);
+        this.timestamp = LocalDateTime.parse(timestamp, DATE_FORMATTER);
         this.longitude = longitude;
         this.latitude = latitude;
         this.filename = filename;
