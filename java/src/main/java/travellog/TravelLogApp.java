fa2011842b9aae87a7a629d325a128ea3c04ddaa
Philip Lourandos <abpl245@absa.co.za>
2016-12-20 10:46:41 +0200

    DEV
    # Added filtering for image files

--- java/src/main/java/travellog/TravelLogApp.java
+++ java/src/main/java/travellog/TravelLogApp.java
@@ -13,6 +13,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -24,20 +29,34 @@ public class TravelLogApp {
 
     private static final Logger LOG = LogManager.getLogger();
     
+    private static final Pattern IMAGES_PATTERN = Pattern.compile("\\.(png|jpg|jpeg)", Pattern.CASE_INSENSITIVE);
+    
     @Parameter(names = {"--input-directory", "-d"}, description = "Directory to scan image files for", required = true)
     private String directory;
     
     @Parameter(names = {"--output", "-o"}, description = "Output path and file name of report", required = true)
     private String reportFile;
 
+    private List<LogInfo> logs;
+    
     public TravelLogApp() {
     }
 
     public void run() {
         Path inputDirectory = Paths.get(directory);
+
+        logs = new ArrayList<>();
+
+        Predicate<Path> fileFilter = (p) -> {
+            final String filename = p.getFileName().toString();
+            
+            Matcher match = IMAGES_PATTERN.matcher(filename);
+            
+            return match.find();
+        };
         
         try {
-            Files.list(inputDirectory).forEach(f -> {
+            Files.list(inputDirectory).filter(fileFilter).forEach(f -> {
                 LOG.info("Processing file: {}", f.toString());
 
                 try {
@@ -52,7 +71,9 @@ public class TravelLogApp {
                     final String latitude = gpsMetaDir.getString(GpsDirectory.TAG_DEST_LATITUDE);
                     final String longitude = gpsMetaDir.getString(GpsDirectory.TAG_DEST_LONGITUDE);
 
-                    LOG.info("Pic taken: {}, GPS: {}, {}", datetime, longitude, latitude);
+                    LOG.info("Pic taken: {}, Longitude: {}, Latitude: {}", datetime, longitude, latitude);
+                    
+                    logs.add(new LogInfo(datetime, longitude, latitude, f.getFileName().toString()));
                 } catch (IOException jpegReadFail) {
                     LOG.error("", jpegReadFail);
                 }
@@ -61,7 +82,11 @@ public class TravelLogApp {
             LOG.error("Unable to list files in directory", invalidDir);
         }
     }
-    
+
+    public List<LogInfo> getLogs() {
+        return logs;
+    }
+           
     public static void main(String[] args) {
         TravelLogApp app = new TravelLogApp();
 
