3d96105adfc6060997e6aa1490c186c3e1d49da2
Philip Lourandos <abpl245@absa.co.za>
2016-12-21 11:18:18 +0200

    DEV
    # Longitude and latitude are now stored as doubles

--- java/src/main/java/travellog/LogInfo.java
+++ java/src/main/java/travellog/LogInfo.java
@@ -8,14 +8,15 @@ import java.time.format.DateTimeFormatter;
  * @author plourand
  */
 public class LogInfo implements Comparable<LogInfo> {
+
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss");
-    
+
     private final LocalDateTime timestamp;
-    private final String longitude;
-    private final String latitude;
+    private final double longitude;
+    private final double latitude;
     private final String filename;
 
-    public LogInfo(final String timestamp, final String longitude, final String latitude, final String filename) {
+    public LogInfo(final String timestamp, final double longitude, final double latitude, final String filename) {
         this.timestamp = LocalDateTime.parse(timestamp, DATE_FORMATTER);
         this.longitude = longitude;
         this.latitude = latitude;
@@ -26,24 +27,20 @@ public class LogInfo implements Comparable<LogInfo> {
         return timestamp;
     }
 
-    public String getLatitude() {
+    public double getLatitude() {
         return latitude;
     }
 
-    public String getLongitude() {
+    public double getLongitude() {
         return longitude;
     }
 
     public String getFilename() {
         return filename;
     }
-    
+
     @Override
     public int compareTo(LogInfo o) {
-        if (LogInfo.class.isAssignableFrom(o.getClass())) {
-            return timestamp.compareTo(o.timestamp);
-        } else {
-            return -1;
-        }
+        return timestamp.compareTo(o.timestamp);
     }
 }
